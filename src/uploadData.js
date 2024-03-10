dd011ffb9aebb33ebc539bba180154fa0a63ecda
CosmiCloud <tybranha@gmail.com>
2024-03-10 16:54:19 -0400

    fixed check context

--- src/uploadData.js
+++ src/uploadData.js
@@ -47,7 +47,7 @@ module.exports = {
         });
 
       let dkg_txn_data = JSON.parse(data.txn_data);
-      if (!dkg_txn_data["@context"]) {
+      if (!dkg_txn_data.hasOwnProperty("@context")) {
         dkg_txn_data["@context"] = "https://schema.org";
       }
 
